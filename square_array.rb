ebc98027a82fac666f4ddada3f17d1703cc3266f
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 20:12:16 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
-def square_array(array)
-  # your code here
-  myArray = Array.new
-  array.each do |square|
-    myArray << square * square
-  end
-
-  return myArray
-end
+# def square_array(array)
+#   # your code here
+#   myArray = Array.new
+#   array.each do |square|
+#     myArray << square * square
+#   end
+# 
+#   return myArray
+# end
 
 def square_array(array)
   array.collect { |square| square * square}
