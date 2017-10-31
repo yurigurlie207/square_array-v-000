aa65ebae1f67ae5455f82493ff7ff62c89494b06
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 20:24:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -8,10 +8,10 @@
 #   return myArray
 # end
 
-def square_array(array)
-  myArray = Array.new
-  myArray.push(array.collect { |square| square * square})
-
-  puts "#{myArray}"
-end
-square_array([1,2,3])
+# def square_array(array)
+#   myArray = Array.new
+#   myArray.push(array.collect { |square| square * square})
+# 
+#   puts "#{myArray}"
+# end
+# square_array([1,2,3])
