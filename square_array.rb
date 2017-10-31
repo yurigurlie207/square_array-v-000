9fe370637243be1b74a48eecb7bc1d007c60256b
Adrienne Yao <adrienne.yao@icloud.com>
2017-10-31 20:24:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
   # your code here
+  myArray = Array.new
+  array.each do |square|
+    myArray << square * square
+  end
+
+  return myArray
 end
+
+# def square_array(array)
+#   myArray = Array.new
+#   myArray.push(array.collect { |square| square * square})
+#
+#   puts "#{myArray}"
+# end
+# square_array([1,2,3])
